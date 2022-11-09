1f998839909c7e4402e453a28a7e1100b1e557ac
Тимофей Чижевский <chizhevskii.tima@gmail.com>
2022-11-09 15:14:56 +0100

    Added (ConfirmEmail)

--- WebAds/Areas/Identity/Controllers/AccountController.cs
+++ WebAds/Areas/Identity/Controllers/AccountController.cs
@@ -1,64 +1,90 @@
-﻿using BLL.Services;
-using Domain.Models;
+﻿using Domain.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Win32;
+using WebAds.Models;
 
 namespace WebAds.Areas.Identity.Controllers
 {
     [Area("Identity")]
     public class AccountController : Controller
-    {
-        private readonly UserServices _userServices;        
+    {   
         private readonly SignInManager<User> _signInManager;
         private readonly UserManager<User> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
 
-        public AccountController(UserServices userServices, SignInManager<User> manager,
-            UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        private readonly IEmailSender _emailService;
+
+        public AccountController(SignInManager<User> manager, UserManager<User> userManager, 
+            RoleManager<IdentityRole> roleManager, IEmailSender emailService)
         {
-            _userServices = userServices;
             _signInManager = manager;
             _userManager = userManager;
             _roleManager = roleManager;
+
+            _emailService = emailService;
         }
 
         public IActionResult Register() => View();
         public IActionResult Login() => View();
 
         [HttpPost]
-        public async Task<IActionResult> Register(User user)
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
         {
-            var result = await _userManager.CreateAsync(user, user.PasswordHash);
-            
+            var user = new User()
+            {
+                Email = model.Email,
+                UserName = model.Name,
+                Surname = model.Surname,
+                PhoneNumber = model.PhoneNumber,
+                CityName = model.CityName,
+                IsMailing = model.IsMailing,
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
             if (result.Succeeded)
             {
-                //await _userManager.AddToRoleAsync(_userServices.Fin, "user");
+                if(await _roleManager.FindByNameAsync("user") == null)
+                    await _roleManager.CreateAsync(new IdentityRole("user"));
 
-                return View("Login");
+                await _userManager.AddToRoleAsync(user, "user");
+                await _signInManager.SignInAsync(user, isPersistent: false);
+
+                var t1 = Request.Scheme;
+                var t2 = Request.Host.Value;
+
+                var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                var confirmLink = Url.Action("Confirm", "EmailConfirm", new { token = token, userEmail = user.Email }, Request.Scheme, Request.Host.Value + "/Identity");
+
+                var link = $"<lable>Please click the link for confirm Email address:</lable><a href='{confirmLink}'>Confirm Email</a>";
+
+                await _emailService.SendEmailAsync(user.Email, "Confirmation Email(WebAd)", link);
+
+                return RedirectToAction("Index", "Home");
             }
-            else
-                return View((object)result.Errors.First().Description);
+            
+            return View(nameof(Register), result.Errors.First().Description);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Login(User user, bool isPersistent)
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
         {
-            try
+            if (ModelState.IsValid)
             {
-                if (user.UserName == null || user.PasswordHash == null)
-                    throw new Exception("UserName or Password equals null!");
-
-                var result = await _signInManager.PasswordSignInAsync(user.UserName, user.PasswordHash, isPersistent, lockoutOnFailure: true);
-
-                if (result != null && result.Succeeded)
-                    return Redirect("~/Home");
+                
+                var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, model.IsPersistent, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
                 else
-                    throw new Exception("Can't find the user!");
+                {
+                    return View(nameof(Login), "User not found!");
+                }
             }
-            catch (Exception ex)
-            {
-                return View(viewName: nameof(Login), model: ex.Message);
-            }
+            return View(nameof(Login), "Error!");
         }
 
         [HttpGet]
---
+++ WebAds/Areas/Identity/Controllers/EmailConfirmController.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAds.Areas.Identity.Controllers
+{
+    [Area("Identity")]
+    public class EmailConfirmController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        private readonly IEmailSender _emailService;
+
+        public EmailConfirmController(UserManager<User> userManager, IEmailSender emailSender)
+        {
+            _userManager = userManager;
+            _emailService = emailSender;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Confirm(string token, string userEmail)
+        {
+            var user = await _userManager.FindByEmailAsync(userEmail);
+
+            if (user != null)
+            {
+                var result = await _userManager.ConfirmEmailAsync(user, token);
+                if(result.Succeeded)
+                {
+                    await _emailService.SendEmailAsync(userEmail, "Confirmation Email(WebAd)", "<p>Your email has been verified!</p>");
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            return View();
+        }
+    }
+}
