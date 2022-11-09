019509dc7609d9dc89e4a5e32a94f9f38446570d
Тимофей Чижевский <chizhevskii.tima@gmail.com>
2022-11-09 17:52:20 +0100

    Edit login and registration methods

---
+++ WebAds/Models/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace WebAds.Models
+{
+    public class RegisterViewModel
+    {
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        
+
+        public string CityName { get; set; }
+
+        public bool IsMailing { get; set; }
+        
+    }
+}
